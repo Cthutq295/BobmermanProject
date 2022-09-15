601e77f0d13900720bf179f70c8ff18653c0440e
Sergey <fsk-2001@mail.ru>
2022-09-15 21:27:38 +0300

    Cleaned Up Character Code
    Removed unused functions, VR code etc

--- Source/Bomberman/BombermanCharacter.h
+++ Source/Bomberman/BombermanCharacter.h
@@ -19,32 +19,17 @@ class ABombermanCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+	
 public:
 	ABombermanCharacter();
 
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
-
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<ABomb> Bomb;
-
-	UPROPERTY(VisibleAnywhere)
-	bool IsReadyToPlaceBomb = true;
 	
-	FTimerHandle Timer;
-
 	UPROPERTY(EditAnywhere, meta = ( ClampMin="1", ClampMax="3"))
 	int BombPlaceCoolDown = 2;
 
 protected:
-
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
 
 	/** Called for forwards/backward input */
 	void MoveForward(float Value);
@@ -55,27 +40,6 @@ protected:
 	void OnPlaceBomb();
 	
 	void OnLeaveGame();
-	
-	UFUNCTION()
-	void ResetCoolDown();
-
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
-
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
 
 protected:
 	// APawn interface
@@ -89,8 +53,16 @@ public:
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 private:
+	
+	UPROPERTY()
 	class UAIPerceptionStimuliSourceComponent* stimulus;
+	
+	bool IsReadyToPlaceBomb = true;
+    	
+	FTimerHandle Timer;
 
 	void setup_stimulus();
+
+	void ResetCoolDown();
 };
 
